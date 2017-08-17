9e79377a91c4f516d0c0181cd9bb6c4ff62b2dd5
flowerinthenight <chew.esmero@mobingi.com>
2017-08-17 17:50:34 +0900

    Fix urls.

--- pkg/cli/constants.go
+++ pkg/cli/constants.go
@@ -12,12 +12,12 @@ const (
 	DevelopmentBaseRegistryUrl = "https://dockereg2.labs.mobingi.com" // dev Docker Registry base url
 
 	// v3 base
-	AlmBaseApiUrl      = "https://alm.mobingi.com"  // production API base url for ALM
-	WaveBaseApiUrl     = "https://wave.mobingi.com" // production API base url for Wave
-	AlmDevBaseApiUrl   = "https://alm.mobingi.com"  // dev API base url for ALM
-	WaveDevBaseApiUrl  = "https://wave.mobingi.com" // dev API base url for Wave
-	AlmTestBaseApiUrl  = "https://alm.mobingi.com"  // test API base url for ALM
-	WaveTestBaseApiUrl = "https://wave.mobingi.com" // test API base url for Wave
+	AlmBaseApiUrl      = "https://alm.mobingi.com"     // production API base url for ALM
+	WaveBaseApiUrl     = "https://wave.mobingi.com"    // production API base url for Wave
+	AlmDevBaseApiUrl   = "https://almdev.mobingi.com"  // dev API base url for ALM
+	WaveDevBaseApiUrl  = "https://wavedev.mobingi.com" // dev API base url for Wave
+	AlmTestBaseApiUrl  = "https://almqa.mobingi.com"   // test API base url for ALM
+	WaveTestBaseApiUrl = "https://waveqamobingi.com"   // test API base url for Wave
 
 	ApiVersion       = "v2" // Mobingi API version
 	DockerApiVersion = "v2" // Docker API version
